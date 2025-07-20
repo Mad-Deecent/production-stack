e92143d6cbb39d907b3beeac573777c8694ad64c
btdeviant <btdeviant@proton.me>
2025-07-20 15:37:33 -0700

    closer

--- tests/scripts/port-forward.sh
+++ tests/scripts/port-forward.sh
@@ -3,12 +3,13 @@
 echo "Waiting for all llmstack pods to be in Running state..."
 
 # Save output
-VAR="${1#curl-}"
+VAR=$1
+NAMESPACE=$2
 [ ! -d "output-$VAR" ] && mkdir "output-$VAR"
 chmod -R 777 "output-$VAR"
 
 # Print router logs
-POD_NAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep '^vllm-deployment-router')
+POD_NAME=$(kubectl get pods -n "$NAMESPACE" --no-headers -o custom-columns=":metadata.name" | grep '^vllm-deployment-router')
 echo "Getting pod $POD_NAME"
 
 kubectl wait --for=condition=ready pod/"$POD_NAME" --timeout=120s
@@ -17,8 +18,8 @@ kubectl logs -f "$POD_NAME" 2>&1 | tee "output-$VAR/router.log" &
 # Loop to check if all llmstack-related pods are in the Running state
 while true; do
     # Get all pods containing "vllm" in their name and extract their STATUS column
-    pod_status=$(kubectl get pods --no-headers | grep "vllm" | awk '{print $3}' | sort | uniq)
-    pod_ready=$(kubectl get pods --no-headers | grep "vllm" | awk '{print $2}' | sort | uniq)
+    pod_status=$(kubectl get pods -n "$NAMESPACE" --no-headers | grep "vllm" | awk '{print $3}' | sort | uniq)
+    pod_ready=$(kubectl get pods -n "$NAMESPACE" --no-headers | grep "vllm" | awk '{print $2}' | sort | uniq)
 
     # If the only unique status is "Running", break the loop and continue
     if [[ "$pod_status" == "Running" ]] && [[ "$pod_ready" == "1/1" ]]; then
@@ -31,9 +32,9 @@ while true; do
 done
 
 # Expose router service
-kubectl patch service vllm-router-service -p '{"spec":{"type":"NodePort"}}'
+kubectl patch service vllm-router-service -n "$NAMESPACE" -p '{"spec":{"type":"NodePort"}}'
 ip=$(minikube ip)
-port=$(kubectl get svc vllm-router-service -o=jsonpath='{.spec.ports[0].nodePort}')
+port=$(kubectl get svc vllm-router-service -n "$NAMESPACE" -o=jsonpath='{.spec.ports[0].nodePort}')
 
 sleep 5
 
